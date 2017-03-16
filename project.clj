14308a3365eb9ae1f0a99c56f5a13ce9c5d3e965
Michael Rose <michael@rosenetwork.net>
2017-03-16 01:42:41 -0700

    current

--- project.clj
+++ project.clj
@@ -3,4 +3,19 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [
+                 [com.rpl/specter "0.13.0"]
+                 [mvxcvi/puget "1.0.1"]
+                 [org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 ;; [org.skummet/clojure "1.7.0-r2"]
+                 [swiss-arrows "1.0.0"]
+                 [com.cemerick/pomegranate "0.3.1"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 ]
+  ;; :main ^:skip-aot hd-schedule-sync
+  :target-path "target/%s"
+  :jvm-opts ["-Xmx200m" "-server"]
+  :repl-options {:port 5000}
+  ;; :profiles {:uberjar {:aot :all}}
+  )
